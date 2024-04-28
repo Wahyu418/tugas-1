43578d0e2db42f9921a97bc46db4a1de90c9e000
Wahyu418 <168348988+Wahyu418@users.noreply.github.com>
2024-04-28 23:23:56 +0700

    Update TB1.py

--- TB1/TB1.py
+++ TB1/TB1.py
@@ -1,31 +1,3 @@
-def hitung_nilai_akhir(tugas, uts, uas):
-    # Menghitung nilai akhir berdasarkan bobot
-    nilai_akhir = (0.25 * tugas) + (0.35 * uts) + (0.40 * uas)
-    return nilai_akhir
-
-def tentukan_nilai_huruf(nilai_akhir):
-    # Menentukan nilai huruf berdasarkan kriteria
-    if nilai_akhir > 85:
-        return "A"
-    elif nilai_akhir > 80:
-        return "A-"
-    elif nilai_akhir > 75:
-        return "B+"
-    elif nilai_akhir > 70:
-        return "B"
-    elif nilai_akhir > 65:
-        return "B-"
-    elif nilai_akhir > 60:
-        return "C+"
-    elif nilai_akhir > 55:
-        return "C"
-    elif nilai_akhir > 50:
-        return "C-"
-    elif nilai_akhir > 30:
-        return "D"
-    else:
-        return "E"
-
 def main():
     print("Program Menghitung Nilai Akhir Mahasiswa")
     print("=======================================")
@@ -33,12 +5,42 @@ def main():
     uts = float(input("Masukkan nilai UTS: "))
     uas = float(input("Masukkan nilai UAS: "))
 
-    nilai_akhir = hitung_nilai_akhir(tugas, uts, uas)
+    nilai_akhir = hitung_nilai(tugas, uts, uas)
     
-    nilai_huruf = tentukan_nilai_huruf(nilai_akhir)
+  
+    nilai_huruf = tentukan_huruf(nilai_akhir)
     
+   
     print(f"Nilai Akhir: {nilai_akhir:.2f}")
     print(f"Nilai Huruf: {nilai_huruf}")
 
-if __name__ == "_main_":
+def hitung_nilai(tugas, uts, uas):
+    nilai = (0.25 * tugas) + (0.35 * uts) + (0.40 * uas)
+    return nilai
+
+def tentukan_huruf(nilai):
+   
+    if nilai > 85:
+        return "A"
+    elif nilai > 80:
+        return "A-"
+    elif nilai > 75:
+        return "B+"
+    elif nilai > 70:
+        return "B"
+    elif nilai > 65:
+        return "B-"
+    elif nilai > 60:
+        return "C+"
+    elif nilai > 55:
+        return "C"
+    elif nilai > 50:
+        return "C-"
+    elif nilai > 30:
+        return "D"
+    else:
+        return "E"
+
+if __name__ == "__main__":
     main()
+
